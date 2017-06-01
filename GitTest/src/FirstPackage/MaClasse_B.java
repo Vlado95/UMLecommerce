190d76bab0c727894b50c7fb983b8715f0fe27dd
Vlado95 <tvradmir@gmail.com>
2017-06-01 11:27:18 +0200

    bit

---
+++ GitTest/src/FirstPackage/MaClasse_B.java
@@ -0,0 +1,13 @@
+package FirstPackage;
+
+public class MaClasse_B {
+
+	String msg;
+	int i;
+	
+	String msg23;
+	int iiii;
+	
+	String ii3; // changement pour le test du 040517 à 14h05
+	MaClasse_A maClasseA; // changement suite au changement de l'adresse mail dans GIT
+}
